dd9a6e0ab26a04e3a2dee5cef0e3c68cf2d2276e
Otoneil Hernández Garzón <hernandezottto@misena.edu.co>
2019-10-20 13:07:24 -0500

    Chore: Se agrga Calculadora como proyecto Java

---
+++ otros ejercicio/Calculadora/src/main/java/com/mycompany/figuras/Calculadora.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.figuras;
+
+/**
+ *
+ * @author investigacion03
+ */
+public class Calculadora {
+    
+    int suma, resta, multiplicacion,division; 
+    
+    public void sumar(int numeroA, int numeroB){
+        suma= numeroA+numeroB;
+    }
+    public void restar(int numeroA, int numeroB){
+        resta= numeroA-numeroB;
+    }
+    public void multiplicar(int numeroA, int numeroB){
+        multiplicacion= numeroA*numeroB;
+    }
+    public void dividir(int numeroA, int numeroB){
+        division= numeroA/numeroB;
+    }
+    
+    
+    public void resultado(int numeroa,int numerob){
+      System.out.println( "Calculadora con métodos globlales"); 
+      System.out.println("el resultodo de la suma "+numeroa+" + "+numerob+" = " + suma);  
+      System.out.println("el resultodo de resta "+numeroa+" - "+numerob+" = "+ resta);   
+      System.out.println("el resultodo de multiplicación "+numeroa+" X "+numerob+" = "+ multiplicacion);   
+      System.out.println("el resultodo de division \n \n"+numeroa+" / "+numerob+" = " + division);   
+    }
+}
