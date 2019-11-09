5e00772377fc0e27f25b61baa77de7a425a7afe4
Otoneil Hernández Garzón <hernandezottto@misena.edu.co>
2019-11-09 15:42:48 -0500

    Chore: Sa agrega un Proyecto CuentaBancaria

---
+++ otros ejercicio/cuentaBacaria/src/cuentabacaria/Persona.java
@@ -0,0 +1,12 @@
+package cuentabacaria;
+
+/**
+ *
+ * @author investigacion03
+ */
+public class Persona {
+    final int documento;
+    public Persona(int NumeroDocumento) {
+         documento=NumeroDocumento;
+    }
+}
